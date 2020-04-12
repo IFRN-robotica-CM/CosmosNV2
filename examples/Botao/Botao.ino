f8d2c703446d35ef35c08885df87dfd8bca38e1b
viniciossilva811 <vinicsilva811@gmail.com>
2020-04-12 17:17:49 -0300

    Comentados exemplos

--- examples/Botao/Botao.ino
+++ examples/Botao/Botao.ino
@@ -1,12 +1,20 @@
-#include "CosmosNV2.h"
+/*
+                          Botao.ino
+  Este código explica como utilizar botoes com esta biblioteca
+  Criado em 12 de Abril de 2020
+  Por Vinícios Silva
+*/
 
-Botao botao(5);
+#include "CosmosNV2.h"                  // Inclui a biblioteca
+
+Botao botao(5);                         // Cria um botao e anexa na porta 5
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);                   // Inicia a comunicação serial
 }
 
 void loop() {
-  boolean estado = botao.lerEstado();
-  Serial.println(estado);
+  boolean estado = botao.lerEstado();   // Faz uma leitura no botao e guarda o resultado na variável "estado" 
+  Serial.println(estado);               // Mostra o valor da variável "estado" no Monitor Serial
+  delay(500);                           // Espera 500ms 
 }
