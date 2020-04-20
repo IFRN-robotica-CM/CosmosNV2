138660a4847d8cae84278b591fef291cbab52316
viniciossilva811 <vinicsilva811@gmail.com>
2020-04-20 20:22:01 -0300

    Adicionado suporte ao Monitor Serial
    Tradução do objeto Serial para português

---
+++ src/MonitorSerial.h
@@ -0,0 +1,15 @@
+
+#ifndef _MonitorSerial_h
+#define _MonitorSerial_h
+
+#include "Arduino.h"
+
+#define MonitorSerial          Serial
+#define iniciar                begin
+#define existeDadosDisponiveis available
+#define imprimir               print
+#define imprimirLinha          println
+#define ler                    read
+#define lerAte                 readStringUntil
+
+#endif
